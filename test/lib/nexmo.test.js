56af2262e7e50ff52d041167f837dabe90a39964
Ian Wu <onlinemad@gmail.com>
2016-09-11 22:53:03 +0800

    Added callback parameter and receipt function for nexmo.

--- test/lib/nexmo.test.js
+++ test/lib/nexmo.test.js
@@ -5,7 +5,7 @@ var nexmo = require('../../index').nexmo;
 var nexmo = new nexmo(config.nexmo);
 
 describe('nexmo', function() {
-  it('success case', function(done) {
+  it.skip('success case', function(done) {
     var payload = {
       to: config.fixture.to,
       text: '[operator] test from nexmo. nexmo 測試簡訊. time ' + Date.now()
@@ -23,6 +23,25 @@ describe('nexmo', function() {
     }
     nexmo.send(payload, function(result) {
       expect(result.status).to.equal('failed');
+      done();
+    });
+  });
+  it('receipt', function(done) {
+    var payload = { 
+      msisdn: '886918361763',
+      to: '889989666777',
+      'network-code': '46697',
+      messageId: '06000000449DC552',
+      price: '0.03580000',
+      status: 'delivered',
+      scts: '1609111016',
+      'err-code': '0',
+      'message-timestamp': '2016-09-11 10:17:39'
+    }
+    nexmo.receipt(payload, function(result) {
+      expect(result.id).to.equal('06000000449DC552');
+      expect(result.provider).to.equal('nexmo');
+      expect(result.raw).to.deep.equal(payload);
       done();
     });
   });
