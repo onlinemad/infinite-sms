36e1612def2dcbf30648609e241d24e18e0f36f8
Ian Wu <onlinemad@gmail.com>
2016-09-23 14:33:57 +0800

    Refactor mitake. Supported receipt function. Supported utf8 character.

--- test/lib/mitake.test.js
+++ test/lib/mitake.test.js
@@ -1,14 +1,14 @@
 var expect = require('chai').expect;
 
-var mitake = require('../../index').mitake;
+var Mitake = require('../../index').mitake;
 
-var mitake = new mitake(config.mitake);
+var mitake = new Mitake(config.mitake);
 
 describe('mitake', function() {
   it('success case', function(done) {
     var payload = {
       to: config.fixture.to,
-      text: '[operator] test from mitake. mitake 測試簡訊. time ' + Date.now()
+      text: '[operator] test from mitake. mitake 測試簡訊 テスト. time ' + Date.now()
     }
     mitake.send(payload, function(result) {
       expect(result.status).to.equal('ok');
@@ -18,12 +18,42 @@ describe('mitake', function() {
   });
   it('missing destination', function(done) {
     var payload = {
-      to: '',
-      text: '[operator] test from mitake. mitake 測試簡訊. time ' + Date.now()
+      to: '+886',
+      text: '[operator] test from mitake. mitake 測試簡訊 テスト. time ' + Date.now()
     }
     mitake.send(payload, function(result) {
       expect(result.status).to.equal('failed');
       done();
     });
+  });
+  it('optional parameter', function(done) {
+    var payload = {
+      to: config.fixture.to,
+      text: '[operator] test from mitake. mitake 測試簡訊 テスト. time ' + Date.now(),
+      options: {
+        response: 'https://operator.sleepnova.org/api/operator/callback_stub'
+      }
+    }
+    mitake.send(payload, function(result) {
+      expect(result.status).to.equal('ok');
+      expect(result.id).to.exist;
+      done();
+    });
+  });
+  it('receipt', function(done) {
+    var payload = { msgid: '$0005EF6C0',
+      dstaddr: '0919919919',
+      dlvtime: '20160923141411',
+      donetime: '20160923141415',
+      statusstr: 'DELIVRD',
+      statuscode: '0',
+      StatusFlag: '4' 
+    }
+    var result = Mitake.receipt(payload);
+    expect(result.id).to.equal('$0005EF6C0');
+    expect(result.provider).to.equal('mitake');
+    expect(result.status).to.equal('DELIVRD');
+    expect(result.raw).to.deep.equal(payload);
+    done();
   });
 })
