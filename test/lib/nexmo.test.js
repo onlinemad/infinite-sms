271523a5249d4a6a690bfceea23df8c419c3a829
Ian Wu <onlinemad@gmail.com>
2020-02-21 14:11:33 +0800

    Refactor nexmo.js to promise style.

--- test/lib/nexmo.test.js
+++ test/lib/nexmo.test.js
@@ -1,47 +1,50 @@
 const expect = require('chai').expect
+const debug = require('debug')('sms')
 
 const Nexmo = require('../../index').nexmo
 
 const nexmo = new Nexmo(config.nexmo)
 
-describe('nexmo', () => {
-  it('success case', (done) => {
-    const payload = {
+describe('nexmo', async () => {
+  it('success case', async () => {
+    let payload = {
       to: config.fixture.to,
       text: `[operator] test for nexmo 測試簡訊 テスト 😀 time ${now()}`
     }
-    nexmo.send(payload, (result) => {
-      expect(result.status).to.equal('ok')
-      expect(result.id).to.exist
-      done()
-    })
+    let result = await nexmo.send(payload)
+    debug('nexmo result', result)
+    expect(result.status).to.equal('ok')
+    expect(result.id).to.exist
   })
-  it('missing destination', (done) => {
-    const payload = {
+  it('missing destination', async () => {
+    let payload = {
       to: '',
       text: `[operator] test from nexmo 測試簡訊 テスト 😀 time ${now()}`
     }
-    nexmo.send(payload, (result) => {
-      expect(result.status).to.equal('failed')
-      done()
-    })
+    let result = await nexmo.send(payload)
+    debug('nexmo result', result)
+    expect(result.status).to.equal('failed')
   })
-  it('optional parameter', (done) => {
-    const payload = {
+  it('balance', async () => {
+    let result = await nexmo.balance()
+    debug('nexmo result', result)
+    expect(result.status).to.equal('ok')
+    expect(result.balance).to.exist
+  })
+  it('optional parameter', async () => {
+    let payload = {
       to: config.fixture.to,
       text: `[operator] test from nexmo 測試簡訊 テスト 😀 time ${now()}`,
       options: {
         callback: config.nexmo.options.callback
       }
     }
-    nexmo.send(payload, (result) => {
-      expect(result.status).to.equal('ok')
-      expect(result.id).to.exist
-      done()
-    })
+    let result = await nexmo.send(payload)
+    expect(result.status).to.equal('ok')
+    expect(result.id).to.exist
   })
-  it('receipt', (done) => {
-    const payload = {
+  it('receipt', () => {
+    let payload = {
       msisdn: '886919919919',
       to: '889989666777',
       'network-code': '46697',
@@ -52,11 +55,10 @@ describe('nexmo', () => {
       'err-code': '0',
       'message-timestamp': '2016-09-11 10:17:39'
     }
-    const result = Nexmo.receipt(payload)
+    let result = Nexmo.receipt(payload)
     expect(result.id).to.equal('06000000449DC552')
     expect(result.provider).to.equal('nexmo')
     expect(result.status).to.equal('delivered')
     expect(result.raw).to.deep.equal(payload)
-    done()
   })
 })
